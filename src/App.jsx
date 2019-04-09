960d4b38a0f5b7c996b0a7c7e452549ff274278c
pmckeegan <pmckeegan@gmail.com>
2019-04-09 22:09:06 +0000

    completes first 2 exercises

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,62 @@
 import React, {Component} from 'react';
+import ChatBar from './ChatBar.jsx'
+import MessageList from './MessageList.jsx'
 
 class App extends Component {
-  render() {
-    return (
-      <h1>Hello React :)</h1>
-    );
-  }
+  // in App.jsx
+componentDidMount() {
+  console.log("componentDidMount <App />");
+  setTimeout(() => {
+    console.log("Simulating incoming message");
+    // Add a new message to the list of messages in the data store
+    const newMessage = {id: 3, username: 'Michelle', content: 'Hello there!'};
+    const messages = this.state.messages.concat(newMessage)
+    // Update the state of the app component.
+    // Calling setState will trigger a call to render() in App and all child components.
+    this.setState({messages: messages})
+  }, 3000);
 }
+
+render() {
+  // more code here..
+}
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentUser: {name: 'Bob'},
+      messages: [
+        {
+          username: 'Bob',
+          content: 'Has anyone seen my marbles?',
+          id: 666
+        },
+        {
+          username: 'Anonymous',
+          content: 'No, I think you lost them. You lost your marbles Bob. You lost them for good.',
+          id:667
+        }
+      ]
+    }
+  }
+  render() { 
+    
+    
+    return (
+      
+      <div>
+        <nav className="navbar">
+        <h3>chat E</h3>
+        <img src="./build/que.gif"></img>
+        </nav>
+        <ChatBar user={this.state.currentUser} />
+        <MessageList messages={this.state.messages} />
+      </div> 
+      
+    );
+  } 
+}
+
+
+
 export default App;
