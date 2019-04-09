9c48842544ee8b58f624c8579ca2ecdc12fd18c6
pmckeegan <pmckeegan@gmail.com>
2019-04-09 22:29:27 +0000

    exercise 2 completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,7 @@ import ChatBar from './ChatBar.jsx'
 import MessageList from './MessageList.jsx'
 
 class App extends Component {
-  // in App.jsx
-componentDidMount() {
-  console.log("componentDidMount <App />");
-  setTimeout(() => {
-    console.log("Simulating incoming message");
-    // Add a new message to the list of messages in the data store
-    const newMessage = {id: 3, username: 'Michelle', content: 'Hello there!'};
-    const messages = this.state.messages.concat(newMessage)
-    // Update the state of the app component.
-    // Calling setState will trigger a call to render() in App and all child components.
-    this.setState({messages: messages})
-  }, 3000);
-}
 
-render() {
-  // more code here..
-}
   constructor(props) {
     super(props);
 
@@ -39,7 +23,25 @@ render() {
       ]
     }
   }
-  render() { 
+
+  // in App.jsx
+  componentDidMount() {
+    console.log("componentDidMount <App />");
+    setTimeout(() => {
+      console.log("Simulating incoming message");
+      // Add a new message to the list of messages in the data store
+      const newMessage = {id: 3, username: 'Michelle', content: 'Hello there!'};
+      const messages = this.state.messages.concat(newMessage)
+      // Update the state of the app component.
+      // Calling setState will trigger a call to render() in App and all child components.
+      this.setState({messages: messages})
+    }, 3000);
+  }
+  
+  render() {
+    <App />// more code here..
+  
+
     
     
     return (
@@ -47,7 +49,7 @@ render() {
       <div>
         <nav className="navbar">
         <h3>chat E</h3>
-        <img src="./build/que.gif"></img>
+        {/* <img src="./build/que.gif"></img> */}
         </nav>
         <ChatBar user={this.state.currentUser} />
         <MessageList messages={this.state.messages} />
